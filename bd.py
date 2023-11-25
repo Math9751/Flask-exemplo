ecf728953ca520a95fe096d0664cf69abbab10b2
Math9751 <matthewbelt.mc@gmail.com>
2023-11-25 16:13:29 -0300

    API com Flask

---
+++ bd.py
@@ -0,0 +1,26 @@
+Albuns = [
+    {
+        "id": 1,
+        "album": "And Justice for All",
+        "artista": "Metallica",
+        "ano": 1988
+    },
+    {
+        "id": 2,
+        "album": "Spreading the Disease",
+        "artista": "Anthrax",
+        "ano": 1985
+    },
+    {
+        "id": 3,
+        "album": "Rust in Peace",
+        "artista": "Megadeth",
+        "ano": 1990
+    },
+    {
+        "id": 4,
+        "album": "Reign in Blood",
+        "artista": "Slayer",
+        "ano": 1986
+    }
+]
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, make_response, jsonify, request
+from bd import Albuns
+
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False 
+
+@app.route('/albuns', methods=['GET'])
+def get_albuns():
+    return make_response(
+        jsonify (
+            mensagem='Lista de álbuns.',
+            dados=Albuns
+            )
+        )
+
+@app.route('/albuns', methods=['POST'])
+def create_album():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        album_data = request.get_json()
+
+        # Adicione o novo álbum à lista
+        Albuns.append(album_data)
+
+        # Retorne o álbum criado
+        return jsonify(
+            mensagem='Álbum adicionado com sucesso.',
+            álbum=album_data
+            ), 201  # 201 Created
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+
+if __name__ == '__main__':
+    app.run(debug=True)
