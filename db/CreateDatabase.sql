f9f3aef3140fab5b68de8101422bc463bd3a1a56
Math9751 <matthewbelt.mc@gmail.com>
2023-11-26 22:48:31 -0300

    SQL

---
+++ db/CreateDatabase.sql
@@ -0,0 +1,19 @@
+USE Disco;
+
+CREATE TABLE albuns (
+    id integer not null auto_increment,
+    album varchar(100),
+    artista varchar(100),
+    ano integer,
+    PRIMARY KEY (id)
+);
+
+SET character_set_client = utf8;
+SET character_set_conection = utf8;
+SET character_set_results = utf8;
+SET collation_conection = utf8;
+
+INSERT INTO albuns (album, artista, ano) VALUES ('And Justice for All', 'Metallica', 1988);
+INSERT INTO albuns (album, artista, ano) VALUES ('Spreading the Disease', 'Anthrax', 1985);
+INSERT INTO albuns (album, artista, ano) VALUES ('Rust in Peace', 'Megadeth', 1990);
+INSERT INTO albuns (album, artista, ano) VALUES ('Reign in Blood', 'Slayer', 1986);
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, make_response, jsonify, request
 from bd import Albuns
 
 
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False 
 
